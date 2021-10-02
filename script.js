1a87f169dc991bc49d8d113777239ea4161168ed
timor23 <timors23@protonmail.com>
2021-10-02 13:22:51 +0300

    progress

--- script.js
+++ script.js
@@ -2,40 +2,54 @@ const axe = document.querySelector(".axe");
 const pickaxe = document.querySelector(".pickaxe");
 const shovel = document.querySelector(".shovel");
 const world = document.querySelector(".gameWorld");
-let rows = document.getElementsByClassName("gridRow");
-let cells = document.getElementsByClassName("cell");
+
 
 
 // createWorld();
-defaultGrid();
+makeRows(16,20);
 
+
+function makeRows(rows, cols) {
+    world.style.setProperty('--grid-rows', rows);
+    world.style.setProperty('--grid-cols', cols);
+    for (let c = 0; c < (rows * cols); c++) {
+      let cell = document.createElement("div");
+      cell.style.background = "#95d7f1";
+      world.appendChild(cell).className = "grid-item";
+    };
+  };
+  
+  makeRows(16, 16);
+
+
+//===========================
 // Creates a default grid sized 16x16
-function defaultGrid() {
-    makeRows(16);
-    makeColumns(20);
-}
+// function defaultGrid() {
+//     makeRows(16);
+//     makeColumns(20);
+// }
 
 // Takes (rows, columns) input and makes a grid
-function makeRows(rowNum) {
+// function makeRows(rowNum) {
 
-    // Creates rows
-    for (r = 0; r < rowNum; r++) {
-        let row = document.createElement("div");
-        world.appendChild(row).className = "gridRow";
-    }
-}
+//     // Creates rows
+//     for (r = 0; r < rowNum; r++) {
+//         let row = document.createElement("div");
+//         world.appendChild(row).className = "gridRow";
+//     }
+// }
 
-// Creates columns
-function makeColumns(cellNum) {
-    for (i = 0; i < rows.length; i++) {
-        for (j = 0; j < cellNum; j++) {
-            let newCell = document.createElement("div");
-            newCell.style.background = "#95d7f1";
-            rows[j].appendChild(newCell).className = "cell";
-        }
+// // Creates columns
+// function makeColumns(cellNum) {
+//     for (i = 0; i < rows.length; i++) {
+//         for (j = 0; j < cellNum; j++) {
+//             let newCell = document.createElement("div");
+//             newCell.style.background = "#95d7f1";
+//             rows[j].appendChild(newCell).className = "cell";
+//         }
 
-    }
-}
+//     }
+// }
 //////=========
 function createWorld() {
     for (let r = 0 ; r < 17 ; r++) {
