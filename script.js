eff965f50afc0e5e8af858104acf1d3d9ad946a8
timor23 <timors23@protonmail.com>
2021-10-03 03:37:29 +0300

    done

--- script.js
+++ script.js
@@ -2,13 +2,36 @@ const axe = document.querySelector(".axe");
 const pickaxe = document.querySelector(".pickaxe");
 const shovel = document.querySelector(".shovel");
 const world = document.querySelector(".gameWorld");
+const tiles = document.querySelector(".tiles");
+const grass = document.querySelector(".grass-zee");
+const dirt = document.querySelector("#dirt");
+const wood = document.querySelector("#wood");
+const rock = document.querySelector("#rock");
+const leaves = document.querySelector("#leaves");
 
-const gridMatrix = {};
-// createWorld();
+
+const GROUND_LEVEL = 10;
+const DEEPEST_LEVEL = 16;
+
 world.style.backgroundColor = "#95d7f1"
+const gridMatrix = {};
+let selected;
+let inventory = {grass: 0, dirt: 0, wood: 0, rock: 0, leaves: 0}
+let invButtons = [axe, pickaxe, shovel, grass, wood, leaves, dirt, rock];
+// createWorld();
+const matrix = [];
 
 createWorld(16, 20);
 createLand();
+createTree(4);
+createTree(11);
+createRocks(15, 1);
+createRocks(16, 2);
+createRocks(17, 3);
+createRocks(18, 4);
+createRocks(19, 5);
+
+console.log(matrix);
 
 
 function createWorld(rows, cols) {
@@ -22,16 +45,17 @@ function createWorld(rows, cols) {
 
         world.appendChild(cell).classList.add("isEmpty");
         gridMatrix[`${row}.${col}`] = cell;
+        // matrix[row][col] = cell;
     }
 }
 
 function createLand() {
-    let row = 10; // grass row
+    let row = GROUND_LEVEL; // grass row
     for (let col = 0; col < 20; col++) {
         gridMatrix[`${row}.${col}`].classList.add("grass");
         gridMatrix[`${row}.${col}`].classList.remove("isEmpty");
     }
-    for (row = 11; row < 16; row++) {
+    for (row = GROUND_LEVEL + 1; row < DEEPEST_LEVEL; row++) {
         for (let col = 0; col < 20; col++) {
             gridMatrix[`${row}.${col}`].classList.add("dirt");
             gridMatrix[`${row}.${col}`].classList.remove("isEmpty");
@@ -39,21 +63,134 @@ function createLand() {
     }
 }
 
+function createTree(col) {
+    for (let r = GROUND_LEVEL - 1; r > GROUND_LEVEL - 4; r--) {
+        gridMatrix[`${r}.${col}`].classList.add("wood");
+        gridMatrix[`${r}.${col}`].classList.remove("isEmpty");
+    }
+    for (let r = 6; r > 3; r--) {
+        for (let c = col - 1; c < col + 2; c++) {
+            gridMatrix[`${r}.${c}`].classList.add("leaves");
+            gridMatrix[`${r}.${c}`].classList.remove("isEmpty");
+
+        }
+    }
+}
+
+function createRocks(col, height) {
+    for (let i = GROUND_LEVEL - 1; i >= GROUND_LEVEL - height; i--) {
+        gridMatrix[`${i}.${col}`].classList.add("rock");
+        gridMatrix[`${i}.${col}`].classList.remove("isEmpty");
+    }
+}
 
 
-
-function collectMaterial(e) {
+function work(e) {
     // let row = e.target.gridRow;
     // console.log(row);
+    let tilePosition = e.target.getAttribute("position").split('.');
+    let row = tilePosition[0];
+    let col = tilePosition[1];
     let target = e.target.classList;
-    let tilePosition = e.target.getAttribute("position");
-    console.log(tilePosition);
-    // console.log(e.target.getAttribute("isEmpty"));
-    if (true) {
-        if (target.contains("dirt") || target.contains("grass")) {
-            e.target.classList.remove("grass", "dirt");
-            e.target.classList.add("isEmpty");
-        }
+    // console.log(gridMatrix);
+    let row_ = row - 1;
+    let tmp = gridMatrix[`${row_}.${col}`];
+    console.log(tmp);
+    switch (selected) {
+        case "shovel":
+            if (target.contains("grass")) {
+                e.target.classList.remove("grass");
+                e.target.classList.add("isEmpty");
+                inventory.grass++;
+                tiles.children[0].style.display = "block";
+                tiles.children[0].innerHTML = `${inventory.grass}`;
+            } else {
+                if (target.contains("dirt")) {
+                    e.target.classList.remove("dirt");
+                    e.target.classList.add("isEmpty");
+                    inventory.dirt++;
+                    tiles.children[1].style.display = "block";
+                    tiles.children[1].innerHTML = `${inventory.dirt}`;
+                }
+            }
+            break;
+        case "axe":
+            if (target.contains("wood")) {
+                e.target.classList.remove("wood");
+                e.target.classList.add("isEmpty");
+                inventory.wood++;
+                tiles.children[2].style.display = "block";
+                tiles.children[2].innerHTML = `${inventory.wood}`;
+            } else {
+                if (target.contains("leaves")) {
+                    e.target.classList.remove("leaves");
+                    e.target.classList.add("isEmpty");
+                    inventory.leaves++;
+                    tiles.children[4].style.display = "block";
+                    tiles.children[4].innerHTML = `${inventory.leaves}`;
+                }
+            }
+            break;
+        case "pickaxe":
+            if (target.contains("rock")) {
+                e.target.classList.remove("rock");
+                e.target.classList.add("isEmpty");
+                inventory.rock++;
+                tiles.children[3].style.display = "block";
+                tiles.children[3].innerHTML = `${inventory.rock}`;
+            }
+            break;
+        case "grass":
+            if (target.contains("isEmpty") && inventory.grass > 0) {
+                e.target.classList.remove("isEmpty");
+                e.target.classList.add("grass");
+                // inventory.grass--;
+                if (--inventory.grass === 0) {
+                    tiles.children[0].style.display = "none";
+                }
+                tiles.children[0].innerHTML = `${inventory.grass}`;
+            }
+            break;
+        case "dirt":
+            if (target.contains("isEmpty") && inventory.dirt > 0) {
+                e.target.classList.remove("isEmpty");
+                e.target.classList.add("dirt");
+                if (--inventory.dirt === 0) {
+                    tiles.children[1].style.display = "none";
+                }
+                tiles.children[1].innerHTML = `${inventory.dirt}`;
+            }
+            break;
+        case "wood":
+            if (target.contains("isEmpty") && inventory.wood > 0) {
+                e.target.classList.remove("isEmpty");
+                e.target.classList.add("wood");
+                if (--inventory.wood === 0) {
+                    tiles.children[2].style.display = "none";
+                }
+                tiles.children[2].innerHTML = `${inventory.wood}`;
+            }
+            break;
+        case "rock":
+            if (target.contains("isEmpty") && inventory.rock > 0) {
+                e.target.classList.remove("isEmpty");
+                e.target.classList.add("rock");
+                if (--inventory.rock === 0) {
+                    tiles.children[3].style.display = "none";
+                }
+                tiles.children[3].innerHTML = `${inventory.rock}`;
+            }
+            break;
+        case "leaves":
+            if (target.contains("isEmpty") && inventory.leaves > 0) {
+                e.target.classList.remove("isEmpty");
+                e.target.classList.add("leaves");
+                if (--inventory.leaves === 0) {
+                    tiles.children[4].style.display = "none";
+                }
+                tiles.children[4].innerHTML = `${inventory.leaves}`;
+            }
+            break;
     }
 
 }
@@ -63,6 +200,61 @@ function collectMaterial(e) {
 
 shovel.addEventListener("click", (element) => {
     shovel.classList.add("selected");
-    world.addEventListener("click", collectMaterial);
-
+    selected = "shovel";
+    world.addEventListener("click", work);
 });
+
+axe.addEventListener("click", (element) => {
+    axe.classList.add("selected");
+    selected = "axe";
+    world.addEventListener("click", work);
+});
+
+pickaxe.addEventListener("click", (element) => {
+    pickaxe.classList.add("selected");
+    selected = "pickaxe";
+    world.addEventListener("click", work);
+});
+
+function unselectAll() {
+    invButtons.forEach(e => {
+        e.style.border = "1px solid white";
+    })
+}
+
+
+grass.addEventListener("click", (element) => {
+    unselectAll();
+    grass.classList.add("selected");
+    selected = "grass";
+    world.addEventListener("click", work);
+});
+
+dirt.addEventListener("click", (element) => {
+    unselectAll();
+    dirt.classList.add("selected");
+    selected = "dirt";
+    world.addEventListener("click", work);
+});
+
+wood.addEventListener("click", (element) => {
+    unselectAll();
+    wood.classList.add("selected");
+    selected = "wood";
+    world.addEventListener("click", work);
+});
+
+rock.addEventListener("click", (element) => {
+    unselectAll();
+    rock.classList.add("selected");
+    selected = "rock";
+    world.addEventListener("click", work);
+});
+
+leaves.addEventListener("click", (element) => {
+    unselectAll();
+    leaves.classList.add("selected");
+    selected = "leaves";
+    world.addEventListener("click", work);
+});
+
